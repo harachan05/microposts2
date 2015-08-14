18564419d32145c00d1841ba22161d8f61f46849
harachan05 <yuki.h.0519@gmail.com>
2015-08-14 03:18:32 +0000

    revised0814

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
- def show # 追加
+  before_action :set_user, only: [:edit, :update]
+  def show 
    @user = User.find(params[:id])
   end
   
@@ -16,11 +17,26 @@ class UsersController < ApplicationController
       render 'new'
     end
   end
-
-  private
-
-  def user_params
-    params.require(:user).permit(:name, :email, :password,
-                                 :password_confirmation)
+  
+  def edit 
   end
+  
+  def update
+    if @user.update(user_params)
+      # 保存に成功した場合はトップページへリダイレクト
+      redirect_to root_path , notice: 'Profileを編集しました'
+    else
+      # 保存に失敗した場合は編集画面へ戻す
+      render 'edit'
+    end
+  end
+  
+  private
+    def user_params
+    params.require(:user).permit(:name, :email, :area, :profile, :password, :password_confirmation)
+    end
+    
+    def set_user
+    @user = User.find(params[:id])
+    end
 end
